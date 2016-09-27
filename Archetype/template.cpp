6eea6e8ddf5db794f949304786bfd26629476d33
irnbru <makubex-0827694@hotmail.co.jp>
2016-09-28 02:36:42 +0900

    programme template

---
+++ Archetype/template.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map> //pair is also included in this library
+#include <string>
+#include <vector>
+
+#define REP(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+
+
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PII;
+
+int main(){
+  
+
+  return 0;
+}
