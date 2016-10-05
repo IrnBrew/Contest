f295b829bdf46d37a962b34902bc7826005bbce5
irnbru <makubex-0827694@hotmail.co.jp>
2016-10-05 14:41:17 +0900

    DP, VL plz

--- AtCoder/ABC044/C.cpp
+++ AtCoder/ABC044/C.cpp
@@ -21,48 +21,29 @@ typedef vector<int> VI;
 typedef vector<ll> VL;
 typedef pair<int, int> PII;
 
-int solve(int N, int A, VI v){
-  int ans = 0;
+ll solve(int N, int A, VI v){
+  ll ans = 0;
 
-  vector<vector<vector<int>>> dp(N+1, vector<vector<int>>(N, vector<int>(N * A)));
-
-  cout << dp.size() << " " << dp[0].size() << " " << dp[0][0].size() << endl;
-  dp[1][0][0] = 1;
-  cout << dp[1][0][0] << endl;
-  REP(i, 0, N+1){
-    REP(j, 0, N){
-      REP(k, 0, N * A + 1){
-        dp[1][0][0] = i;
-        cout << i << j << k << endl;
-        dp[i][j][k] = 0;
-      }
-    }
-  }
+  vector<vector<VL>> dp(N+1, vector<VL>(N+1, VL(N * A + 1)));
 
   REP(i, 0, N+1){
-    REP(j, 0, N){
+    REP(j, 0, N+1){
       REP(k, 0, N * A + 1){
-        if(i == 0 && j == 0 && k == 0){
-          dp[0][0][0] = 1;
-        } else if(v[i] > k && i > 0 && j > 0){
-          cout << "1,i, j, k: " << i << " " << j << " " << k << endl;
-          cout << "dp[i][j][k]: " << dp[i][j][k] << endl;
-          dp[i][j][k] = dp[i][j-1][k];
+        if(j == 0 && k == 0){
+          dp[i][0][0] = 1;
+        } else if(v[i] > k && j > 0){
+          dp[i][j][k] = dp[i-1][j][k];
         } else if(v[i] <= k && i > 0 && j > 0){
-          cout << "2,i, j, k-v[i]: " << i << " " << j << " " << k-v[i] << endl;
-          cout << dp[i-1][j-1][k-v[i]] << endl;
-          cout << dp[i][j-1][k] << endl;
-          dp[i][j][k] = dp[i][j-1][k] + dp[i-1][j-1][k-v[i]];
+          dp[i][j][k] = dp[i-1][j][k] + dp[i-1][j-1][k-v[i]];
         } else {
-          cout << "3,i, j, k: " << i << " " << j << " " << k << endl;
           dp[i][j][k] = 0;
         }
       }
     }
   }
-
-  REP(i, 0, N){
-    ans += dp[N][i][i*N];
+  
+  REP(i, 1, N+1){
+    ans += dp[N][i][i*A];
   }
 
   return ans;
@@ -73,14 +54,18 @@ int main(){
 
   cin >> N >> A;
 
-  VI v(N);
+  VI v(N+1);
+  v[0] = 0;
 
-  int i = 0;
-  while(cin >> v[i]){
-    i++;
+  // int i = 1;
+  // while(cin >> v[i]){
+  //   i++;
+  // }
+  REP(i, 1, N+1){
+    cin >> v[i];
   }
 
-  int ans = solve(N, A, v);
+  ll ans = solve(N, A, v);
 
   cout << ans << endl;
 
