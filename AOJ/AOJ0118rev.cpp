f9a7de9d24d6bf33ae10ee8904dcacf02ab23df8
irnbru <makubex-0827694@hotmail.co.jp>
2016-10-08 17:54:12 +0900

    AOJ

--- AOJ/AOJ0118rev.cpp
+++ AOJ/AOJ0118rev.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 typedef long long int ll;
 typedef vector<int> VI;
-typedef vector<vector<int>> VII;
 typedef vector<ll> VL;
 typedef pair<int, int> PII;
 
@@ -25,30 +24,31 @@ typedef pair<int, int> PII;
 
 /*------------------------------------------------------------------------------*/
 
-int x[4] = {-1, 0, 1, 0};
-int y[4] = {0, -1, 0, 1};
+void dfs(char c, pair<int, int> cur, vector<vector<char>> &p, int H, int W){
+  int x[4] = {-1, 0, 1, 0};
+  int y[4] = {0, -1, 0, 1};
+  p[cur.first][cur.second] = '.';
 
-void dfs(char c, int u, int v, int W, int H, vector<vector<char>> &p){
-  p[u][v] = '.';
   REP(i, 0, 4){
-    int nx, ny;
-    nx = u + x[i];
-    ny = v + y[i];
-    if(nx > 0 && ny > 0 && nx <= W && ny <= H && c == p[nx][ny]){
-      dfs(c, nx, ny, W, H, p);
+    int nx = cur.second + x[i];
+    int ny = cur.first + y[i];
+    if(nx >= 0 && nx < W && ny >= 0 && ny < H && c == p[ny][nx]){
+      dfs(c, {ny, nx}, p, H, W);
     }
   }
- return;
+  return;
 }
 
 int solve(int H, int W, vector<vector<char>> p){
   int ans = 0;
-  REP(i, 1, W+1){
-    REP(j, 1, H+1){
-      if(p[i][j] != '.'){
+  char c = '.';
+
+  REP(i, 0, H){
+    REP(j, 0, W){
+      if('.' != p[i][j]){
         ans++;
-        char c = p[i][j];
-        dfs(c, i, j, W, H, p);
+        c = p[i][j];
+        dfs(c, {i, j}, p, H, W);
       }
     }
   }
@@ -57,23 +57,20 @@ int solve(int H, int W, vector<vector<char>> p){
 
 int main(){
   int H, W;
-
   while(1){
     cin >> H >> W;
-
     if(H == 0 && W == 0){
       break;
     }
-    vector<vector<char>> p(W+1, vector<char>(H+1));//p[x][y] (x, y)
-
-    REP(i, 1, W+1){
-      REP(j, 1, H+1){
+    vector<vector<char>> p(H, vector<char>(W));
+    REP(i, 0, H){
+      REP(j, 0, W){
         cin >> p[i][j];
       }
     }
     int ans = solve(H, W, p);
     cout << ans << endl;
   }
-  
+
   return 0;
 }
