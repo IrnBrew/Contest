472338ace519dd79f9f8645f43aa10a0e5e262fb
irnbru <makubex-0827694@hotmail.co.jp>
2016-11-15 15:43:13 +0900

    [AC] POJ1862: Easy calculation.

---
+++ POJ/POJ1862.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map> //pair is also included in this library
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+//#include <unordered_map>
+#include <vector>
+
+#define REP(i, s, n) for(int i = (int)(s); i < (int)(n); i++)
+#define fst first
+#define snd second
+#define MP make_pair //incase c++11 or later is not available
+
+using namespace std;
+
+typedef long long int ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+
+//prefer define statement
+//for reason that const int ver. of INF will raise a silly warning (unused variable)
+#define INF (INT_MAX/3)
+#define LIM_INF (INT_MAX)
+//const int INF = INT_MAX/3;
+
+/*------------------------------------------------------------------------------*/
+
+int main(){
+  int N;
+  cin >> N;
+  vector<double> v(N);
+  REP(i, 0, N){
+    cin >> v[i];
+  }
+  sort(v.begin(), v.end(), greater<double>());
+  double ans = v[0];
+  REP(i, 1, N){
+    ans = 2 * sqrt(ans*v[i]);
+  }
+
+  cout << fixed << setprecision(3) << ans << endl;
+  return 0;
+}
